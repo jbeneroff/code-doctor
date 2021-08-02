b34a5a60168619a25c1b99d145a17ec58b467253
Jared Beneroff <jaredbeneroff@Jareds-MacBook-Pro.local>
2021-08-02 10:35:52 -0400

    user backend

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from "express"
+import cors from "cors"
+import morgan from "morgan"
+import db from "./db/connection.js"
+import routes from './routes/index.js'
+
+const app = express()
+const PORT = process.env.PORT || 4567
+
+app.use(express.json())
+app.use(cors())
+app.use(morgan("dev"))
+
+app.use('/api', routes)
+
+app.get('/', (req, res) => res.send("<h1>Dev Home</h1>"))
+
+db.on("connected", () => {
+  console.log("Connected to MongoDB")
+  app.listen(PORT, console.log(`Connected to port: ${PORT}`))
+})
