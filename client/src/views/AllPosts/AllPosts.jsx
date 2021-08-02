80f3192af865f12a344889989e0f0ff2e01fc915
Stefanie <stefanien001@gmail.com>
2021-08-02 13:50:13 -0500

    added boilerplate code to each file

---
+++ client/src/views/AllPosts/AllPosts.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../components/Layout/Layout'
+
+export default function AllPosts() {
+  return (
+    <div>
+      <Layout>
+        All Posts
+      </Layout>
+    </div>
+  )
+}
