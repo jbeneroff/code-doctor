4581e1d80d09c4b6241ec816dc26b3db957a86be
Ziyad Hammad <ziyad.hammad@outlook.com>
2021-08-05 16:40:14 -0400

    Almost Done with footer and home

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -15,27 +15,24 @@ export default function Home(props) {
           <h1>Welcome to Code Doctor!</h1>
           <h3>A Secure Platform for Web Developers to Post Code and Get Help from Others in the Community! </h3>
           <Link to="/posts">
-        <button className="b2">See Community Posts</button>
+        <button className="b1">See Community Posts</button>
           </Link>
        
         </div>
 
         <div className="home-2">
-          <h1>Not a Member Yet? Get Signed Up Below!</h1>
+
+          <h1>Do You Want to Start Sharing Your Code?</h1>
+          <h3>Create an Account & Get Started!</h3>
           <Link to="/sign-up">
-           <button className="b1">Sign Up</button>
+           <button className="b2">Sign Up</button>
           </Link>
-         
-      
-          <div className="color-box">
-            
-            
+
           </div>
        
         </div>
        
-        
-      </div>
+     
    </Layout>
   )
 }
