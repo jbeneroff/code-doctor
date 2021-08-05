9d1d9d83862c804b2b949fcb440134e76f371136
Jared Beneroff <jaredbeneroff@Jareds-MacBook-Pro.local>
2021-08-05 14:29:07 -0400

    search changes

--- client/src/views/AllPosts/AllPosts.jsx
+++ client/src/views/AllPosts/AllPosts.jsx
@@ -5,45 +5,55 @@ import { getAllPosts } from '../../services/posts.js'
 
 export default function AllPosts(props) {
   const [posts, setPosts] = useState([])
-  const [filteredPosts, setFilteredPosts] = useState([])
+  const [filteredPosts, setFilteredPosts] = useState(posts)
   const [input, setInput] = useState("")
 
   useEffect(() => {
     const fetchPosts = async () => {
       let data = await getAllPosts()
-      // console.log(data)
       setPosts(data)
+      setFilteredPosts(data)
     }
     fetchPosts()
   }, [])
 
-  useEffect(() => {
-    setFilteredPosts(posts)
-  }, [posts])
+  // useEffect(() => {
+  //   setFilteredPosts(posts)
+  // }, [posts])
 
-  useEffect(() => {
-    if (input) {
-      setFilteredPosts(posts.filter((post) => {
-        if (post.title.includes(input)) {
-          return post
-        }
-        return null
-      }))
-    } else {
-      setFilteredPosts(posts)
-    }
-  }, [posts, input])
+  // useEffect(() => {
+  //   if (input) {
+  //     setFilteredPosts(posts.filter((post) => {
+  //       if (post.title.includes(input)) {
+  //         return post
+  //       }
+  //       return null
+  //     }))
+  //   } else {
+  //     setFilteredPosts(posts)
+  //   }
+  // }, [posts, input])
 
-  const handleChange = async (e) => {
-    setInput(e.target.value)
+    // const handleChange = async (e) => {
+  //   setInput(e.target.value)
+  // }
+
+  const handleChange = () => {
+    const result = posts.filter((post) => {
+      return (post.title.toLowerCase().includes(input.toLowerCase()))
+    })
+    setFilteredPosts(result)
   }
-
+  
+  const matches = filteredPosts.filter((item) => item.title.toLowerCase().includes(input.toLowerCase()))
 
   return (
     <div>
       <Layout user={props.user} setUser={props.setUser}>
-        <input type="text" onChange={handleChange} placeholder="Search" />
-        {filteredPosts.map((post, key) => {
+        <form onSubmit={handleChange}>
+          <input value={input} type="text" placeholder="Search" onChange={(e) => setInput(e.target.value)} />
+        </form>
+        {matches.map((post, key) => {
           return (
             <Link key={post._id} to={`/posts/${post._id}`}>
               <h3>{post.title}</h3>
