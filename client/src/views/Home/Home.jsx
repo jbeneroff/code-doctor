67b3bf43a5518eba229b2255d296c8f95872ffb3
Ziyad Hammad <ziyad.hammad@outlook.com>
2021-08-05 21:24:40 -0400

    Done with Landing Page(Home)

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -5,34 +5,39 @@ import "./Home.css"
 
 
 
+
 export default function Home(props) {
   return (
-    <Layout user={props.user} setUser={props.setUser}>
-      <div className="wrapper">
-
-        <div className="home-1">
-
-          <h1>Welcome to Code Doctor!</h1>
-          <h3>A Secure Platform for Web Developers to Post Code and Get Help from Others in the Community! </h3>
+    
+ 
+      <div className="section">
+      <h1>Welcome to Code Doctor!</h1>
+      <h3>A Secure Platform for Web Developers to Post Code and Get Help from Others in the Community! </h3>
           <Link to="/posts">
         <button className="b1">See Community Posts</button>
           </Link>
        
-        </div>
+    
+       
 
-        <div className="home-2">
-
-          <h1>Do You Want to Start Sharing Your Code?</h1>
-          <h3>Create an Account & Get Started!</h3>
+          <h2>Do You Want to Start Sharing Your Code?</h2>
+          <h4>Create an Account & Get Started!</h4>
           <Link to="/sign-up">
            <button className="b2">Sign Up</button>
+      </Link>
+      <p>OR</p>
+      <Link to="/sign-in">
+           <button className="b3">Sign In</button>
           </Link>
 
-          </div>
+        
        
-        </div>
+       
+      </div>
+     
+    
        
      
-   </Layout>
+
   )
 }
