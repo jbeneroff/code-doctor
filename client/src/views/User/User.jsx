014b90d2c54321820c7408202550cfe3f5b0671d
Stefanie <stefanien001@gmail.com>
2021-08-06 14:40:24 -0500

    user page and all posts updated

--- client/src/views/User/User.jsx
+++ client/src/views/User/User.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import Layout from "../../components/Layout/Layout"
 import { getUserPosts } from '../../services/posts'
+import { verify } from '../../services/users'
 
 export default function User(props) {
   const [userPosts, setUserPosts] = useState([])
@@ -10,29 +11,34 @@ export default function User(props) {
   useEffect(() => {
     const fetchUserPosts = async () => {
       let data = await getUserPosts(id)
-      // console.log(data)
       setUserPosts(data)
     }
     fetchUserPosts()
   }, [id])
 
+  useEffect(() => {
+    const fetchUser = async () => {
+      const data = await verify()
+      props.setUser(data)
+    }
+    fetchUser()
+    // eslint-disable-next-line
+  }, [id])
 
   return (
     <Layout user={props.user} setUser={props.setUser}>
-      {/* <h2>{props.user.username}</h2> */}
-      {/* <h3>{`${props.user.username}'s Posts`}</h3> */}
+      <h2>{props.user?.username}</h2>
+      <h3>{`${props.user?.username}'s Posts:`}</h3>
       {userPosts.map((post, key) => {
         return (
-          <div key={post._id}>
-            <Link to={`/posts/${post._id}`}>
-              <h3>{post.title}</h3>
-              {/* add in logic for formatting code for pmvp */}
-              {/* <p>{post.content}</p> */}
+          <div key={post._id} className="postarea">
+            <Link className='post-link' to={`/posts/${post._id}`}>
+              <h3 className='post-title' >{post.title}</h3>
+              <p className="time">{`Posted at ${post.createdAt.slice(11, 16)} on ${post.createdAt.slice(5, 10)}-${post.createdAt.slice(0, 4)}`}</p>
             </Link>
           </div>
         )
       })}
-      {/* <h3>{`${props.user.username}'s Comments`}</h3> */}
     </Layout>
   )
 }
