f7b432d282e4444f15b663b63c83565879b21a14
Kheyyon <parktwins1@gmail.com>
2021-08-09 15:02:20 -0400

    I made a landing page hopefully no merge conflicts

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import { Link } from "react-router-dom"
 import "./Home.css"
-
-
-
-
+import Navbar from '../../components/Navbar/Navbar'
 export default function Home() {
   return (
     
- 
       // <div className="section">
       // <h1>Welcome to Code Doctor!</h1>
       // <h3>A Secure Platform for Web Developers to Post Code and Get Help from Others in the Community! </h3>
@@ -25,17 +21,39 @@ export default function Home() {
       //      <button className="b3">Sign In</button>
       //     </Link>
       // </div>
-     <div className="body">
+   
+  <div className="body">
     <div className="theStory">
-      <div className="Homebg">
+        <div className="Homebg">
+        <Navbar/>
           <div className="image-area">
             <div className="slogan-area">
-        <h1 className="slogan">We turn 404s into 200s again </h1>
-        </div>
+      
+        <p className="para">Join a community of like-minded developers who have a unifying passion for for functional and D.R.Y. code.</p>
+              <p className="para1">See what's up in the code doctor community</p>
+              <Link to="/posts">
+         <button className="b1">See Community Posts</button>
+           </Link>
+           <h2>Do You Want to Start Sharing Your Code?</h2>
+        <h4>Create an Account & Get Started!</h4>
+        <div className="BTN">
+           <Link to="/sign-up">
+            <button className="b2">Sign Up</button>
+       </Link>
+       <p className="para">OR</p>
+       <Link to="/sign-in">
+            <button className="b3">Sign In</button>
+                </Link>
+          
+              <br />
+              <h1 className="slogan">We turn 404s into 200s again!</h1>
+              </div>    
             </div>
       </div>
       </div>
-      
+      </div>
+    </div>
+  )
       {/* <div className="content">
         <h2 className="description">A Secure Platform for Web Developers to Post Code and Get Help from Others in the Community!</h2>
       
@@ -53,25 +71,22 @@ export default function Home() {
       <div className="img2">
       <img className="owners" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcpftHNILHez9wYrHFa3qFE1mvc-20sWDYBfwqHipSUTfkuMlhdl3EzNPQIsxO4QSyd1Y&usqp=CAU" alt="owners?"/>
       </div> */}
-
-      <div className="CTA">
-        <p>Join a community of like-minded developers who have a unifying passion for for functional and D.R.Y. code.</p>
-        <br/>
+        {/* <div className="CTA">
            <Link to="/posts">
          <button className="b1">See Community Posts</button>
            </Link>
-           <h2 className="CTAH2">Do You Want to Start Sharing Your Code?</h2>
-        <h4 className="CTACA">Create an Account & Get Started!</h4>
+           <h2>Do You Want to Start Sharing Your Code?</h2>
+        <h4>Create an Account & Get Started!</h4>
         <div className="BTN">
            <Link to="/sign-up">
             <button className="b2">Sign Up</button>
        </Link>
-       <p>OR</p>
+       <p className="para">OR</p>
        <Link to="/sign-in">
             <button className="b3">Sign In</button>
           </Link>
           </div>
        </div>
-      </div>
-  )
+      </div> */}
+  
 }
