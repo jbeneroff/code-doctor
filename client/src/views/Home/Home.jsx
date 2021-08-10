1186d82ff4e3baa4c7a2790775a00568adae57cf
Jared Beneroff <jaredbeneroff@Jareds-MacBook-Pro.local>
2021-08-09 20:52:20 -0400

    home fixes

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -13,20 +13,20 @@ export default function Home() {
           <Navbar/>
           <div className="image-area">
             <div className="slogan-area">
-        <p className="para">Join a community of like-minded developers who have a unifying passion for for functional and D.R.Y. code.</p>
-              <p className="para">See what's up in the code doctor community</p>
+              <p className="para">Join a community of like-minded developers who have a unifying passion for functional and D.R.Y. code.</p>
+              <p className="para">See what's up in the code doctor community.</p>
               <Link to="/posts">
-         <button className="b1">See Community Posts</button>
-           </Link>
-           <h2 className="question">Do You Want to Start Sharing Your Code?</h2>
-        <h4 className="engage">Create an Account & Get Started!</h4>
-        <div className="BTN">
-           <Link to="/sign-up">
-            <button className="b1">Sign Up</button>
-       </Link>
-       <p className="para">OR</p>
-       <Link to="/sign-in">
-            <button className="b1">Sign In</button>
+                <button className="b1">See Community Posts</button>
+              </Link>
+              <h2 className="question">Want to Start Sharing Your Code?</h2>
+              <h4 className="engage">Get Started!</h4>
+              <div className="BTN">
+                <Link to="/sign-up">
+                  <button className="b1">Sign Up</button>
+                </Link>
+                <p className="para">OR</p>
+                <Link to="/sign-in">
+                  <button className="b1">Sign In</button>
                 </Link>
                 <br />
                 <h1 className="slogan">We turn 404s into 200s again!</h1>
