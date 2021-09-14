996f650e2011323e45eb058d6014e51a1d7ef35a
Jared Beneroff <jaredbeneroff@Jareds-MacBook-Pro.local>
2021-09-14 14:50:58 -0400

    home edit

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -13,13 +13,13 @@ export default function Home() {
           <Navbar/>
           <div className="image-area">
             <div className="slogan-area">
-              <p className="para">Join a community of like-minded developers who have a unifying passion for functional and D.R.Y. code.</p>
-              <p className="para">See what's up in the code doctor community.</p>
+              <p className="para">Join a community of like-minded developers who have a unifying passion for exceptional code.</p>
+              <p className="para">See what's up in the Code Doctor community.</p>
               <Link to="/posts">
                 <button className="b1">See Community Posts</button>
               </Link>
-              <h2 className="question">Want to Start Sharing Your Code?</h2>
-              <h4 className="engage">Get Started!</h4>
+              {/* <h2 className="question">Want to Start Sharing Your Code?</h2> */}
+              <h4 className="engage">Get Started</h4>
               <div className="BTN">
                 <Link to="/sign-up">
                   <button className="b1">Sign Up</button>
@@ -29,7 +29,7 @@ export default function Home() {
                   <button className="b1">Sign In</button>
                 </Link>
                 <br />
-                <h1 className="slogan">We turn 404s into 200s again!</h1>
+                {/* <h1 className="slogan">We turn 404s into 200s again!</h1> */}
               </div>    
             </div>
           </div>
